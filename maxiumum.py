699499daa6c0e2eb5bbd8dec6093308c665d4ad3
Piyush Motwani <100508072+realpiyushmotwani@users.noreply.github.com>
2023-08-03 09:17:34 +0530

    Add files via upload

---
+++ maxiumum.py
@@ -0,0 +1,8 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it
+import math
+maxi=-1
+num = [1,4,10,34,4,3]
+for i in num:
+    maxi = max(maxi,i)
+print(maxi)
